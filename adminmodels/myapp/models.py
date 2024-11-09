3d5e7ef5f4e8ddeb517a191a8c76cfe678e58461
j1ere <jeremywambua106@gmail.com>
2024-11-09 05:47:07 +0300

    Book model and git ignore

--- adminmodels/myapp/models.py
+++ adminmodels/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+#models go here
+class Book(models.Model):
+    title = models.CharField(max_length=200, null=True)
+    author = models.CharField(max_length=100, null=True)
+    publish_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    
