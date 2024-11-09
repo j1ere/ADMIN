f365d9a7b0de3358eb4106ca0e53133648326188
j1ere <jeremywambua106@gmail.com>
2024-11-09 06:03:00 +0300

    intermediate model registration

--- adminmodels/myapp/admin.py
+++ adminmodels/myapp/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Book
 
 #register models here
-admin.site.register(Book)
+#using ModelAdmin class
+class BookAdmin(admin.ModelAdmin):
+    list_display=('title', 'author', 'publish_date') #columns to show in admin 
+    search_fields=('title', 'author') #add search functionality
+    list_filter=('publish_date',) #filter by , as a tuple
+
+admin.site.register(Book,BookAdmin)
